acfdddc9305a67d55c5ccddfccb5bd04cd98f7cf
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-10-05 15:39:56 -0500

    It just magic

--- main.go
+++ main.go
@@ -2,36 +2,38 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/mdlayher/apcupsd"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 	log.Println("Hello There Again")
-	resp, err := http.Get("http://10.0.0.251:9595")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(body))
-	http.HandleFunc("/info", hello)
+	http.HandleFunc("/unreal", unreal)
+	http.HandleFunc("/beef", beef)
 	http.ListenAndServe(":8090", nil)
 
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func unreal(w http.ResponseWriter, req *http.Request) {
 	log.Println("Got Request")
 	c, err := apcupsd.Dial("tcp", "10.0.0.251:3551")
+	if err != nil {
+		writeData(Data{"Unknown", 0.0, 0.0, 0.0}, w)
+		return
+	}
+	status, err := c.Status()
+	if err != nil {
+		writeData(Data{"Error", 0.0, 0.0, 0.0}, w)
+		return
+	}
+	usesage := (float64(status.NominalPower) * (0.1 * status.LoadPercent)) / 10
+	writeData(Data{status.Status, status.LoadPercent, status.NominalPower, usesage}, w)
+}
+
+func beef(w http.ResponseWriter, req *http.Request) {
+	log.Println("Got Request")
+	c, err := apcupsd.Dial("tcp", "10.0.0.224:3551")
 	if err != nil {
 		writeData(Data{"Unknown", 0.0, 0.0, 0.0}, w)
 		return
